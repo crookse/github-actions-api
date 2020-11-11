7764c6c07de0f968988e05ab5b2b38be3fcd1610
Eric Crooks <eric.crooks@gmail.com>
2020-11-11 00:29:34 -0500

    build the Authorization header

--- twitter.ts
+++ twitter.ts
@@ -1,3 +1,5 @@
+import { hmac } from "https://denopkg.com/chiefbiiko/hmac/mod.ts";
+
 const args = Deno.args.slice();
 const apiKey = args[0];
 const apiSecret = args[1];
@@ -5,21 +7,49 @@ const accessToken = args[2];
 const accessTokenSecret = args[3];
 const tweet = args[4];
 
-const oauthHeader = [
-  `oauth_consumer_key="${apiKey}"`,
-  `oauth_token="${apiSecret}"`,
-  `oauth_signature_method="HMAC-SHA1"`,
-  `oauth_timestamp="${new Date().getTime().toString()}"`,
-  `oauth_nonce="${new Date().getTime().toString()}"`,
-  `oauth_version="1.0"`,
-  `oauth_signature="PrrDjbjuG%2FS4lL1jM2rdLiL9VYY%3D"`
+let sigPart1 = `POST&${
+  encodeURIComponent("https://api.twitter.com/1.1/statuses/update.json")
+}&`;
+let sigPart2 = [
+  `oauth_consumer_key=${apiKey}`,
+  `oauth_nonce=asdfasfasdfasdf`,
+  `oauth_signature_method=HMAC-SHA1`,
+  `oauth_timestamp=${Math.floor(new Date().getTime() / 1000).toString()}`,
+  `oauth_token=${accessToken}`,
+  `oauth_version=1.0`,
+  `${tweet}`,
 ];
-
-const res = await fetch("https://api.twitter.com/1.1/statuses/update.json?status=" + tweet, {
-  method: 'POST', // or 'PUT'
-  headers: {
-    "Authorization": "OAuth " + oauthHeader.join(","),
-  },
+const sigPart2Encoded = sigPart2.map((value: string) => {
+  return encodeURIComponent(value).replace(/!/g, "%21");
 });
+sigPart2Encoded[sigPart2Encoded.length - 1] = encodeURIComponent("status=") +
+  encodeURIComponent(sigPart2Encoded[sigPart2Encoded.length - 1]);
+
+const key = `${apiSecret}&${accessTokenSecret}`;
+const message = sigPart1 + sigPart2Encoded.join("%26");
+const oAuthSignature = getOAuthSignature(key, message);
+
+function getOAuthSignature(key: string, message: string): string | Uint8Array {
+  return hmac("sha1", key, message, "utf8", "base64") + "%3D";
+}
+
+sigPart2.pop();
+sigPart2.push("oauth_signature=" + oAuthSignature);
+
+console.log(sigPart2);
+
+const oAuthHeader = sigPart2.join(",");
+
+console.log(oAuthHeader);
+
+const res = await fetch(
+  "https://api.twitter.com/1.1/statuses/update.json?status=" + tweet,
+  {
+    method: "POST",
+    headers: {
+      "Authorization": "OAuth " + oAuthHeader,
+    },
+  },
+);
 
 console.log(await res.json());
